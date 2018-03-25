9bc94ba1bd2d253fd3772c0f8743eb9ccb6b93aa
Victor Feijo <victor.feijoa@gmail.com>
2018-03-25 19:11:43 -0300

    Better home page view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+function ready(event) {
+  $('input[type=radio][name=searchRadio]').change(function() {
+    if (this.value === 'simple') {
+      $('.simple-form').show();
+      $('.advanced-form').hide();
+    }
+    else if (this.value === 'advanced') {
+      $('.simple-form').hide();
+      $('.advanced-form').show();
+    }
+  });
+}
+
+$(document).on('turbolinks:load', ready);
